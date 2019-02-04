473ebe06c082ce97749890ab753cb3e8409ca168
Logan Wohlers <lawohlers@gmail.com>
2019-02-04 21:41:04 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,7 +7,11 @@ def second_challenge
       hero_friends: []
       }, 
     :capulet => {
-      #your key/value pairs here!
+     patriarch: {}, 
+      matriarch: {}, 
+      hero: {}, 
+      heroine_friends: []
+      
       }}
 
   
